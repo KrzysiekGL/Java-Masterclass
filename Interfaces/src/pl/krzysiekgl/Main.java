1b1bd303a66d33fe161216130d4435c0260ff74a
krzysiekgl <krzysztof@szczerbowski.pw>
2019-02-21 23:44:58 +0100

    Part 1 complete

--- Interfaces/src/pl/krzysiekgl/Main.java
+++ Interfaces/src/pl/krzysiekgl/Main.java
@@ -3,6 +3,11 @@ package pl.krzysiekgl;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    ITelephone timsPhone;
+	    timsPhone = new DeskPhone(123467);
+
+	    timsPhone.powerOn();
+	    timsPhone.callPhone(123467);
+	    timsPhone.answer();
     }
 }
