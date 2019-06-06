c2df1dd8d5dacf76aca605ebd576be8bfbbb9211
KrzysiekGL <krzysiek@twoflower.localdomain>
2019-06-06 19:52:53 +0200

    Add buttons and text field which are connectet with a controller

--- Java_FX/Events/src/sample/Controller.java
+++ Java_FX/Events/src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    
+    @FXML      //points to the fxml file in which textField element is stored
+    private TextField textField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button secondButton;
+    
+    @FXML
+    public void onButtonClicked(ActionEvent event) {            //event - to determine which control was triggered
+        System.out.println("Hello " + textField.getText());
+        System.out.println("The following button was pressed: " + event.getSource());
+        textField.setText("");
+    }
+    
 }
